72ca94a3114864e0b97efb396edeb677d90b83a3
xiao1995 <1050149954@qq.com>
2024-01-12 19:50:14 +0800

    实现初始化 element 主流程

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -2,7 +2,13 @@ import { h } from '../../lib/guide-mini-vue.esm.js'
 export const App = {
   render() {
     // ui
-    return h('div', 'hi,' + this.msg)
+    return h(
+      'div',
+      { id: "root", class: ["red", "hard"] },
+      // 'hi,mini-vue'
+      [h("p", { class: "red" }, "hi"), h("p", { class: "blue" }, "hi")]
+    )
+
   },
   setup() {
     //   composition api
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -3,10 +3,34 @@ import { createComponentInstance, setupComponent } from "./component"
 export function render(vnode, container) {
     patch(vnode, container)
 }
+
 function patch(vnode, container) {
     // 去处理组件
     // 判断 是不是 element
-    processComponent(vnode, container)
+    if (typeof vnode.type === "string") {
+        processElement(vnode, container)
+    } else if ((vnode.type)) {
+        processComponent(vnode, container)
+    }
+
+}
+function processElement(vnode, container) {
+    const el = document.createElement(vnode.type)
+    const { children } = vnode
+
+    if (typeof children === "string") {
+        el.textContent = children
+    } else if (Array.isArray(children)) {
+        children.forEach(v => {
+            patch(v, el)
+        })
+    }
+    const { props } = vnode
+    for (const key in props) {
+        const val = props[key]
+        el.setAttribute(key, val)
+    }
+    container.append(el)
 }
 function processComponent(vnode: any, container: any) {
     mountComponent(vnode, container)
