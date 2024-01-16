eedd4e229e27caccb58ea969af01da3444110093
xiao1995 <1050149954@qq.com>
2024-01-16 23:05:00 +0800

    【vue.js设计与实现】object式组件加载实现

--- example/DesignAndImplementation/main.js
+++ example/DesignAndImplementation/main.js
@@ -1,4 +1,4 @@
-// 普通对象
+// 普通虚拟dom
 // const vnode = {
 //   tag: 'div',
 //   props: {
@@ -8,18 +8,33 @@
 //   },
 //   children: 'click me',
 // }
-
-const MyComponent = function () {
-  return {
-    tag: 'div',
-    props: {
-      onClick: () => {
-        alert('hello')
+// // function式组件
+// const MyComponent = function () {
+//   return {
+//     tag: 'div',
+//     props: {
+//       onClick: () => {
+//         alert('hello')
+//       },
+//     },
+//     children: 'click me',
+//   }
+// }
+// // object式组件
+const MyComponent = {
+  rerder() {
+    return {
+      tag: 'div',
+      props: {
+        onClick: () => {
+          alert('hello')
+        },
       },
-    },
-    children: 'click me',
-  }
+      children: 'click me',
+    }
+  },
 }
+// function式组件
 const vnode = {
   tag: MyComponent,
 }
@@ -28,6 +43,8 @@ function renderer(vnode, container) {
   if (typeof vnode.tag === 'string') {
     mountElement(vnode, container)
   } else if (typeof vnode.tag === 'function') {
+    mountComponent(vnode, container)
+  } else if (typeof vnode.tag === 'object') {
     mountComponent(vnode, container)
   }
 }
@@ -48,7 +65,8 @@ function mountElement(vnode, container) {
   container.appendChild(el)
 }
 function mountComponent(vnode, container) {
-  const subtree = vnode.tag()
+  // const subtree = vnode.tag()
+  const subtree = vnode.tag.rerder()
   renderer(subtree, container)
 }
 const rootContainer = document.querySelector('#app')
