3688af8a3ae46ffaac13ac1893cd76828dcf38c7
xiao1995 <1050149954@qq.com>
2024-01-25 00:00:06 +0800

    响应式系统的实现-提供用于注册副作用函数的机制

--- example/DesignAndImplementation/effect.js
+++ example/DesignAndImplementation/effect.js
@@ -1,3 +1,5 @@
+// 用一个全局变量存储被注册的副作用函数
+let activeEffect
 
 // 存储副作用函数的桶
 const bucket = new Set()
@@ -10,28 +12,35 @@ const obj = new Proxy(data, {
   // 拦截读取操作
   get(target, key) {
     // 将副作用函数 effect 添加到存储副作用的函数的桶中
-    bucket.add(effect)
+    if (activeEffect) {
+      bucket.add(activeEffect)
+    }
+
     return target[key]
   },
   set(target, key, newVal) {
     // 设置属性值
     target[key] = newVal
     // 把副作用函数 从桶里取出并执行
-    bucket.forEach((fn) => {
-      fn()
-    })
+    bucket.forEach((fn) => fn())
     // 返回 true 代表设置操作成功
     return true
   },
 })
 
 // 副作用函数
-function effect() {
-  document.body.innerText = obj.text
+function effect(fn) {
+  activeEffect = fn
+  fn()
 }
 
-effect()
+effect(
+  // 匿名函数
+  () => {
+  console.log('effect run')
+  document.body.innerText = obj.text
+})
 
 setTimeout(() => {
   obj.text = 'hello vue3'
-}, 1000);
+}, 1000)
